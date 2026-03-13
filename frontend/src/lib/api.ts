5ad4ed49f5d06426bb5fa6a86b56ba103ab26479
whentox <kissbalazskiss04@gmail.com>
2026-03-13 17:41:51 +0100

    API URL frissitese a Render szerverre

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/v1';
+const API_URL = 'https://thesis-2026-backend.onrender.com/api/v1';
 
 export const api = axios.create({
   baseURL: API_URL,
